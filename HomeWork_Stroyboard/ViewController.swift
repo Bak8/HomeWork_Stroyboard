51473ec2d22abcf66fdef15d030b166020367dbf
Bak8 <ataibegaliev@gmail.com>
2022-11-15 15:07:56 +0600

    1

--- HomeWork_Stroyboard/ViewController.swift
+++ HomeWork_Stroyboard/ViewController.swift
@@ -9,9 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextFieldNumber: UITextField!
+    
+    @IBOutlet weak var TextFieldDegree: UITextField!
+    
+    @IBOutlet weak var Result: UILabel!
+    
+    
+   
+    var number = ""
+    var degree = ""
+    
+    
+    
+    
+    @IBAction func buton(_ sender: Any) {
+        number = TextFieldNumber.text!
+        degree = TextFieldDegree.text!
+        
+        if let numberInt = Int(TextFieldNumber.text!){
+            number = "\(numberInt)"
+            if let degreeInt = Int(TextFieldDegree.text!){
+                degree = "\(degreeInt)"
+            
+                func calculation() {
+                var calcResult = 1
+                for i in 1...degreeInt {
+                    calcResult *= numberInt
+                }
+                Result.text = ("\(calcResult)")
+                }
+                
+                calculation()
+                
+            }else{
+                Result.text=("Введите число в поле степень")
+            }
+        } else {
+            Result.text=("Введите число в поле число")
+        }
+    }
+    
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
